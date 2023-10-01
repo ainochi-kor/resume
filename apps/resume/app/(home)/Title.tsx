0f80cd2310d88af47ff7c9a9f28e954e659dfe0f
ainochi-kor <kthrkdals@gmail.com>
2023-10-01 16:47:46 +0900

    이게 SSG가 되나?

--- apps/resume/app/(home)/Title.tsx
+++ apps/resume/app/(home)/Title.tsx
@@ -1,12 +1,18 @@
 import BorderBox from "@/components/BorderBox";
 import { Typography } from "@/components/Typography";
+import dayjs from "dayjs";
 import React from "react";
 
-const Title: React.FC = () => {
+interface TitleProps {
+  data: string;
+}
+
+const Title: React.FC<TitleProps> = () => {
+  const date = dayjs().format("YYYY.MM.DD hh:mm:ss");
   return (
     <BorderBox>
       <Typography className="text-xl">RESUME</Typography>
-      <Typography className="text-xs">Last updated</Typography>
+      <Typography className="text-xs">Last updated: {date}</Typography>
     </BorderBox>
   );
 };
